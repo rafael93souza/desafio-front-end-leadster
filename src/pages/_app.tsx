8eae964eb601fe8bd9a0f403593801f97ed61263
Rafael Souza <rafael93.souzasfc5@gmail.com>
2023-06-25 20:51:12 -0300

    :recycle: refactor: Added context providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,23 @@ import { Header } from '@/components/Header'
 import  GlobalStyle from '../styles/global'
 import type { AppProps } from 'next/app';
 import { FilterProvider } from '@/contexts/FilterContext';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from '@/services/queryClient';
+import { VideoProvider } from '@/contexts/VideosContext';
+import { StyleSheetManager } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <FilterProvider>
-            <Header/>
-            <Component {...pageProps} />
-            <GlobalStyle />
-        </FilterProvider>
-
-        
-      
-     
+  return   (
+    <QueryClientProvider client={queryClient}>
+        <VideoProvider>
+            <FilterProvider>
+                <StyleSheetManager shouldForwardProp={(prop) => prop !== 'theme'}>
+                    <Header/>
+                    <Component {...pageProps} />
+                    <GlobalStyle />
+                </StyleSheetManager>
+            </FilterProvider>
+        </VideoProvider>
+    </QueryClientProvider>  
+  )  
 }
