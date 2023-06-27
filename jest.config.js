7db5baa6aa135943079a9c666185b4e1bebdd2b1
Rafael Souza <rafael93.souzasfc5@gmail.com>
2023-06-27 17:38:40 -0300

    :test_tube: test:  Configuration file test jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const nextJest = require('next/jest')
+ 
+const createJestConfig = nextJest({ dir: './' })
+ 
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  testEnvironment: "jsdom",
+}
+ 
+module.exports = createJestConfig(customJestConfig)
